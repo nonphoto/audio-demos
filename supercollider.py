dfa303fb7ba992992beb46563f41749ad3b60212
Jonas Luebbers <jonaskluebbers@gmail.com>
2017-07-05 00:38:26 -0400

    Add supercollider demos

---
+++ supercollider.py
@@ -0,0 +1,19 @@
+import OSC
+import time
+import random
+
+client = OSC.OSCClient()
+client.connect(('127.0.0.1', 57110))
+
+firstMessage = OSC.OSCMessage()
+firstMessage.setAddress("/event")
+firstMessage.append(220)
+client.send(firstMessage)
+
+time.sleep(2)
+
+secondMessage = OSC.OSCMessage()
+secondMessage.setAddress("/event")
+secondMessage.append(880)
+secondMessage.append(0)
+client.send(secondMessage)
